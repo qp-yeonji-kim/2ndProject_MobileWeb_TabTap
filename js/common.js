10913135a19c3de6a2ffc53e27dd16fd7e845ddc
yeon ji_kim <60783510+qp-yeonji-kim@users.noreply.github.com>
2020-11-11 16:13:58 +0900

    index-navigation

--- js/common.js
+++ js/common.js
@@ -4,25 +4,16 @@ $(document).ready(function () {
   var $gnb = $('#gnb');
   $('#menu_btn').on('click', function () {
     if ($(this).hasClass('act_left')) { //닫기
-      $nav.stop().animate({
-        left: '100%'
-      }, 300, function () {
-        $(this).css({
-          display: 'none'
-        }).find('#gnb > li.on').removeClass('on').children('ul').stop().slideUp();
+      $nav.stop().animate({left: '100%'}, 300, function () {
+        $(this).css({display: 'none'}).find('#gnb > li.on').removeClass('on').children('ul').stop().slideUp();
       });
       $(this).removeClass('act_left').find('.blind-b').text('메뉴 열기');
     } else {
       $(this).addClass('act_left').find('.blind-b').text('메뉴 닫기');
       var $first = $gnb.find('[data-link="first"]');
       var $last = $gnb.find('[data-link="last"]');
-      $nav.css({
-        display: 'block'
-      }).stop().animate({
-        left: '20%'
-      }, 300, function () {
-        $first.focus();
-      });
+      $nav.css({display: 'block'}).stop().animate({left: '20%'}, 300, function () {
+        $first.focus();});
       $first.on('keydown', function (e) {
         console.log(e.keyCode);
         //shift key, keyCode는 어디서 나온거지?
@@ -38,22 +29,21 @@ $(document).ready(function () {
         }
       });
     }
-
-    /* #gnb depth2 내려오기 */
-
-    $gnb.find('li ul').hide();
-    $gnb.find('>li >a').on('click', function () {
-      $gnb.find('>li').removeClass('on').children('ul').hide();
-      $(this).next().show().parent().addClass('on');
-      if ($(this).next().length === 0) {
-        location.href = $(this).attr('href');
-      } else {
-        $(this).parent().siblings().removeClass('on').find('ul').stop().slideUp();
-        $(this).next().stop().slideToggle().parent().toggleClass('on');
-      }
-      return false;
-    });
-
     return false;
   });
+
+  /* #gnb depth2 내려오기 */
+  $gnb.find('li ul').hide();
+  $gnb.find('>li >a').on('click', function () {
+    $gnb.find('>li').removeClass('on').children('ul').hide();
+    $(this).next().show().parent().addClass('on');
+    if ($(this).next().length === 0) {
+      location.href = $(this).attr('href');
+    } else {
+      $(this).parent().siblings().removeClass('on').find('ul').stop().slideUp();
+      $(this).next().stop().slideToggle().parent().toggleClass('on');
+    }
+    return false;
+  });
+
 });
